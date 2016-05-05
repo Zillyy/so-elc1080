6783c064c23e1d88e8aeda339ca9c18a77283ce5
zillyy <lhmedeiros0@gmail.com>
2016-05-05 13:51:47 -0300

    Arquivos tree1 e tree2 reescritos para correção de bugs

---
+++ luis-tree1-2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+static pid_t cria_fork(void){
+	pid_t pid;
+
+	if((pid = fork()) < 0){
+		fprintf(stderr, "Fork de pid %d falhou\n", getpid());
+		exit(-1);
+	}
+	return pid;
+}
+
+void cria_processos(int num_niveis){
+	int i = 0;
+    for (i = 0; i < num_niveis; i++){
+        if (cria_fork() == 0 || cria_fork() == 0){
+            continue;
+        }
+        break;
+    }
+
+    fprintf(stderr, "Tenho PID = %d e PPID = %d\n", getpid(), getppid());
+
+    while (wait(0) > 0);
+    sleep(1.5);
+
+    fprintf(stderr, "Final: %d\n", getpid());
+    
+    return;
+}
+
+int main(){
+	clock_t t_ini, t_fim;
+	t_ini = clock();
+
+	pid_t aux = getpid();
+	fprintf(stderr, "Inicio: %d\n", aux);
+	
+	cria_processos(2);
+	
+	if(getpid() == aux){
+		t_fim = clock();
+		printf("Tempo de execucao: %f segundos\n",((float)(t_fim - t_ini)/CLOCKS_PER_SEC));
+	}
+
+    return 0;
+}
